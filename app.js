abefc444401d0807d09f9c6f5f36f1c6c1375a9f
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-12-14 16:29:25 +0500

    black and white button

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const cols = document.querySelectorAll('.columns')
 function setRandomColors() {
     cols.forEach(columns => {
         const text = columns.querySelector('h2')
+        const button = columns.querySelector('button')
         const color = chroma.random()
 
         text.textContent = color
         columns.style.background = color
 
         setTextColor(text, color)
+        setTextColor(button, color)
     })
 }
 
