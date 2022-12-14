6e10a91e686662a5f5908c658e5459464d58b2a8
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-12-14 16:27:22 +0500

    black and white text

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const cols = document.querySelectorAll('.columns')
 
-function generateRandomColor() {
-    const hexCodes = '0123456789ABCDEF'
-    let color = ''
-    for ( let i = 0; i < 6; i++) { 
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-    return '#' + color
-}
-
 function setRandomColors() {
     cols.forEach(columns => {
         const text = columns.querySelector('h2')
-        const color = generateRandomColor()
+        const color = chroma.random()
 
         text.textContent = color
         columns.style.background = color
+
+        setTextColor(text, color)
     })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+
 }
 
 setRandomColors()
