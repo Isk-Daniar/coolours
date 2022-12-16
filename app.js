09c63576441ae94e37e7829f719dc3d221095251
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-12-16 15:43:13 +0500

    locked colors

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const cols = document.querySelectorAll('.columns')
 
 document.addEventListener('keydown', (event) => {
+    event.preventDefault()
     if (event.code.toLowerCase() ==="space"){
         setRandomColors()
     }
-}
-)
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if (type === 'lock') {
+        const node = 
+            event.target.tagName.toLowerCase() === 'i'
+            ? event.target
+            : event.target.children[0]
+
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
+    }
+})
+
 
 function setRandomColors() {
     cols.forEach(columns => {
+        const isLocked = columns.querySelector('i').classList.contains('fa-lock')
         const text = columns.querySelector('h2')
         const button = columns.querySelector('button')
         const color = chroma.random()
+
+        if (isLocked) {
+            return
+        }
 
         text.textContent = color
         columns.style.background = color
