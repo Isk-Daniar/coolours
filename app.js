cf9d76ef48fbd869679d9f0ea9efb38bbc22d229
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-12-16 16:20:25 +0500

    save colors

--- app.js
+++ app.js
@@ -27,16 +27,28 @@ function copyToClickboard(text) {
     return navigator.clipboard.writeText(text)
 }
 
-function setRandomColors() {
-    cols.forEach(columns => {
+function setRandomColors(isInitial) {
+    const colors = isInitial ? getColorsFromHash() : []
+
+    cols.forEach((columns, index) => {
         const isLocked = columns.querySelector('i').classList.contains('fa-lock')
         const text = columns.querySelector('h2')
         const button = columns.querySelector('button')
-        const color = chroma.random()
-
+        
         if (isLocked) {
+            colors.push(text.textContent)
             return
         }
+
+        const color = isInitial 
+        ? colors[index]
+            ? colors[index]
+            : chroma.random()
+        : chroma.random()
+
+        if (!isInitial){
+            colors.push(color)
+            }
 
         text.textContent = color
         columns.style.background = color
@@ -44,12 +56,27 @@ function setRandomColors() {
         setTextColor(text, color)
         setTextColor(button, color)
     })
+
+    saveColorsHash(colors)
 }
 
 function setTextColor(text, color) {
     const luminance = chroma(color).luminance()
     text.style.color = luminance > 0.5 ? 'black' : 'white'
-
 }
 
-setRandomColors()
+function saveColorsHash(colors = []) {
+    document.location.hash = colors.map((columns) => {
+        return columns.toString().substring(1)
+    })
+    .join('-')
+}
+
+function getColorsFromHash() {
+    if(document.location.hash.length > 1) {
+        return document.location.hash.substring(1).split('-').map(color => '#' + color)
+    }
+    return []
+}
+
+setRandomColors(true)
